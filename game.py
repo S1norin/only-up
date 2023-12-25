2f1ce01e08d25004851cbf1cf6d43e5a5cd88d48
boss of this python game development <assfucker@gmail.com>
2023-12-25 17:54:44 +0700

    something's done(nothing)

--- game.py
+++ game.py
@@ -3,7 +3,11 @@ import pygame
 from platform import Sas
 
 def create_starfield():
-
+    for y in range(10, 590, 80):
+        x = randrange(400)
+        oleg = Sas(x, y, all_spice)
+        oleg1 = Sas(x + 80, y, all_spice)
+        oleg2 = Sas(x - 80, y, all_spice)
 
 
 if __name__ == '__main__':
@@ -18,9 +22,20 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                oleg = Sas(randrange(400), randrange(600), all_spice)
+                if event.button == 3:
+                    print('here')
+                    screen.fill((0, 0, 0))
+                    for sprite in all_spice:
+                        sprite.rect = sprite.rect.move(sprite.rect.x + 10, sprite.rect.y )
+
+                else:
+                    # oleg in his heaven, all's right with project
+                    # oleg = Sas(randrange(400), randrange(600), all_spice)
+
+                    create_starfield()
+
                 all_spice.draw(screen)
-                print("oleg")
+
 
         pygame.display.flip()
     pygame.quit()
