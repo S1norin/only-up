23dedf3d8974de740a0a1dd4725da0a1836eb8d8
Siskorin <3@mail.ru>
2023-12-25 16:48:57 +0700

    platform implemetned

--- game.py
+++ game.py
@@ -1,15 +1,26 @@
+from random import randrange
 import pygame
+from platform import Sas
+
+def create_starfield():
+
+
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Doodle Moodle')
-    size = width, height = 400, 800
+    size = width, height = 400, 600
     screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
     running = True
     while running:
+        all_spice = pygame.sprite.Group()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                oleg = Sas(randrange(400), randrange(600), all_spice)
+                all_spice.draw(screen)
+                print("oleg")
+
         pygame.display.flip()
     pygame.quit()
