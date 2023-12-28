2e7083b9a97a6982b7bff5a138acc685d21722f6
Siskorin <uwu@owo.com>
2023-12-28 16:11:52 +0700

    Character created

--- caratel.py
+++ caratel.py
@@ -1,8 +1,19 @@
+from platform import load_image
+import pygame
+WIDTH, HEIGHT = 25, 40
 GRAVITATION = 1
 
 
-class Character:
-    def __init__(self):
+
+class Sans:
+    def __init__(self, x, y, spice_group):
+        self.x, self.y = x, y
+        original_image = load_image("character.png")
+        transfromed_image = pygame.transform.scale(original_image, (WIDTH, HEIGHT))
+        self.sprite = pygame.sprite.Sprite(spice_group)
+        self.sprite.image = transfromed_image
+        self.sprite.rect = self.sprite.image.get_rect()
+        self.sprite.rect.x, self.sprite.rect.y = self.x, self.y
         self.x = 100
         self.y = 100
         self.vert_velocity = 0
--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 from random import randrange
 import pygame
 from platform import Sas
+from caratel import Sans
+
+from time import sleep
 
 def create_starfield():
     for y in range(10, 590, 80):
@@ -8,8 +11,10 @@ def create_starfield():
         oleg = Sas(x, y, all_spice)
         oleg1 = Sas(x + 80, y, all_spice)
         oleg2 = Sas(x - 80, y, all_spice)
+        oleg3 = Sans(200, 300, sans_group) # Андей Санс
 
 
+speed = 60
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Doodle Moodle')
@@ -17,26 +22,39 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     running = True
     all_spice = pygame.sprite.Group()
+    sans_group = pygame.sprite.Group()
+    clock = pygame.time.Clock()
+    create_starfield()
+    movement = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 3:
-                    screen.fill((0, 0, 0))
-                    for sprite in all_spice:
-                        if sprite.rect.y > 550:
-                            sprite.kill()
-                        sprite.rect = sprite.rect.move(0, 50)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                for sans in sans_group:
+                    movement += 1
+                    if movement == 10:
+                        sans.rect = sans.rect.move(-1, 0)
+                        movement = 0
+            else:
+                for sans in sans_group:
+                    movement += 1
+                    if movement == 10:
+                        sans.rect = sans.rect.move(1, 0)
+                        movement = 0
 
-                else:
-                    # oleg in his heaven, all's right with project
-                    # oleg = Sas(randrange(400), randrange(600), all_spice)
+        if pygame.sprite.spritecollideany(sans_group.sprites()[0], all_spice):
+            sans_inside = True
 
-                    create_starfield()
-
-                all_spice.draw(screen)
-
-
+        clock.tick(speed)
+        speed += 1
+        for sprite in all_spice:
+            if sprite.rect.y > 550:
+                sprite.kill()
+            sprite.rect = sprite.rect.move(0, 1)
+        screen.fill((0, 0, 0))
+        all_spice.draw(screen)
+        sans_group.draw(screen)
         pygame.display.flip()
-    pygame.quit()
+pygame.quit()
