c73ff04a6580de6651239500b72b76a9d09ef2c0
igmih <igmih206@gmail.com>
2024-01-02 14:30:32 +0700

    arrow buttons control added

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
-
 GRAVITATION = 4
 STANDARD_JUMP_SPEED = 4
 SCREEN_SIZE = (1000, 1000)
+MAX_HOR_SPEED = 3
 from random import randrange
 import pygame
 from platform import Sas
@@ -39,7 +39,8 @@ if __name__ == '__main__':
 
     # Time and physics
     clock = pygame.time.Clock()
-    clock.tick(60)
+
+    hor_acceleration = 0
     while running:
         for event in pygame.event.get(): # Exit check
             if event.type == pygame.QUIT:
@@ -47,21 +48,20 @@ if __name__ == '__main__':
 
             # Interaction with main character
             for sans in sans_group:
-                if event.type == pygame.MOUSEMOTION:  # moving character with mouse
-                    relative_mouse_motion = pygame.mouse.get_rel()[0]
-                   # sans.rect.x = pygame.mouse.get_pos()[0] - (oleg.width) / 2
-                    print(center_of_screen)
-                    what_is_love = relative_mouse_motion - center_of_screen[0]
-                    sans.rect = sans.rect.move(relative_mouse_motion, 0)
-                    if relative_mouse_motion > 0:
-                        oleg.hor_velocity = round(abs(oleg.vert_velocity))
-                    elif relative_mouse_motion < 0:
-                        oleg.hor_velocity = round(-abs(oleg.vert_velocity))
-                    print(oleg.vert_velocity)
+                if event.type == pygame.KEYDOWN:  # moving character with arrow buttons
+                    if pygame.key.get_pressed()[pygame.K_LEFT]:
+                        hor_acceleration = -0.05
+                    elif pygame.key.get_pressed()[pygame.K_RIGHT]:
+                        hor_acceleration = 0.05
+                elif event.type == pygame.KEYUP:
+                    hor_acceleration = 0
+
+        if abs(oleg.hor_velocity + hor_acceleration) <= MAX_HOR_SPEED:
+            oleg.hor_velocity += hor_acceleration
 
         # Gravitation and inertion
         for sans in sans_group:
-            sans.rect = sans.rect.move((oleg.hor_velocity, oleg.vert_velocity))
+            sans.rect = sans.rect.move((round(oleg.hor_velocity), oleg.vert_velocity))
 
         if oleg.vert_velocity >= 0:
             # Колизия засчтиывается, даже если ты ты сталкиваешься с блоком лбом. Нужно уменьшать хитбокс, чтобы он был только в ногах
